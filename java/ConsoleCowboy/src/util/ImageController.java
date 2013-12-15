9a536d56a2d3d75eb4ac79ea97fc54733cfd12ae
Patrick Chou <pChou19@gmail.com>
2013-12-14 19:40:50 -0500

    Load image from project directory depending on OS type

--- java/ConsoleCowboy/src/util/ImageController.java
+++ java/ConsoleCowboy/src/util/ImageController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package util;
 
 import java.awt.image.BufferedImage;
@@ -16,33 +15,61 @@ import javax.imageio.ImageIO;
  * @author Brandon
  */
 public class ImageController {
-    
-    /*
-    @TODO
-    Fix this so it works regardless of locations
-    */
-    public BufferedImage cmd() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\cmd50.png"));
+
+    String directory = System.getProperty("user.dir");
+    String OS = System.getProperty("os.name").toLowerCase();
+
+    public BufferedImage cmd() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\cmd50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//cmd50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
-    public BufferedImage netscape() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\netscape50.png"));
+
+    public BufferedImage netscape() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\netscape50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//netscape50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
-    public BufferedImage java() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\java50.png"));
+
+    public BufferedImage java() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\java50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//java50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
-    public BufferedImage aol() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\aol50.png"));
+
+    public BufferedImage aol() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\aol50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//aol50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
-    public BufferedImage floppy() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\floppy50.png"));
+
+    public BufferedImage floppy() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\floppy50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//floppy50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
-    public BufferedImage cd() throws IOException{
-        BufferedImage img = ImageIO.read(new File("C:\\Users\\Brandon\\Documents\\NetBeansProjects\\ConsoleCowboy\\src\\assets\\cd50.png"));
+
+    public BufferedImage cd() throws IOException {
+        String fullDirectory = directory + "\\src\\assets\\cd50.png";
+        if (OS.startsWith("mac")) {
+            fullDirectory = directory + "//src//assets//cd50.png";
+        }
+        BufferedImage img = ImageIO.read(new File(fullDirectory));
         return img;
     }
 }
